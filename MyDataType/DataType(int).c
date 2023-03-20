4ae4ce3668e044c92c8ab44f219a775a7b90df13
mokwongame <ictplusud@gmail.com>
2023-03-20 11:40:42 +0900

    long long 자료형 연습

--- MyDataType/DataType(int).c
+++ MyDataType/DataType(int).c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 void main()
 {
@@ -11,4 +12,31 @@ void main()
 	printf("%d %d, ", j, k);
 	// () 의미: 형 변환; 8 바이트 자료형을 4 바이트(int)로 변환
 	printf("저장 공간 크기 = %d", (int)sizeof(i)); // sizeof(int)
+	puts("======"); // puts: put string; 문자열 하나 출력 + Enter도 출력; 포맷은 지원하지 않음
+	// 자료형으로 표현할 수 있는 최소값과 최대값은 limits.h
+	printf("정수 최소: %d ~ 최대: %d", INT_MIN, INT_MAX);
+
+	// 짧은 정수
+	puts("====");
+	puts("짧은 정수");
+	short i1 = 10; // short int
+	printf("%d, ", i1);
+	printf("저장 공간 크기 = %d, ", (int)sizeof(i1));
+	printf("short 정수 최소: %d ~ 최대: %d", SHRT_MIN, SHRT_MAX);
+
+	// 긴 정수
+	puts("====");
+	puts("긴 정수");
+	long i2 = 10; // long int
+	printf("%d, ", i2);
+	printf("저장 공간 크기 = %d, ", (int)sizeof(i2));
+	printf("long 정수 최소: %d ~ 최대: %d", LONG_MIN, LONG_MAX);
+
+	// 긴 긴 정수
+	puts("====");
+	puts("긴 긴 정수");
+	long long i3 = 10; // long long int
+	printf("%lld, ", i3); // %lld: long long decimal
+	printf("저장 공간 크기 = %lld, ", sizeof(i3));
+	printf("long long 정수 최소: %lld ~ 최대: %lld", LLONG_MIN, LLONG_MAX);
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "int 자료형 연습"
+git commit -m "long long 자료형 연습"
 git push -u origin master
