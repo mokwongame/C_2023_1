31a21d3901b785b7624daf617035670a5b53874a
mokwongame <ictplusud@gmail.com>
2023-03-27 09:56:09 +0900

    MyConst 프로젝트 추가

---
+++ MyConst/Const.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+	// 변수 선언
+	char ch = 'a'; // 'a'는 문자 상수; 리터럴(literal, 문자 자체)
+	int i = 10; // 10은 숫자(정수) 상수; 리터럴(literal, 문자 자체)
+	double x = 1.2345; // 1.2345는 숫자(실수) 상수; 리터럴(literal, 문자 자체)
+	double y1 = 12e10; // e는 10의 지수(exponent); 12e10 = 12*10^10
+	double y2 = 12e+10; // 12e+10 = 12*10^10
+	double y3 = 12e-10; // 12e+10 = 12*10^-10
+
+	long long j = 10ll; // ll: long long; l: long
+	// 1 I l
+	long long k = 20LL; // LL: long long
+	float z = -45.67f; // f: float
+}
+
