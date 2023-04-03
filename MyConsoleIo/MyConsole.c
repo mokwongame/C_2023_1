00cc2e16004ec7ade617d748a0f9903a699ce179
mokwongame <ictplusud@gmail.com>
2023-04-03 09:39:39 +0900

    글자색, 배경색 변경 방법

--- MyConsoleIo/MyConsole.c
+++ MyConsoleIo/MyConsole.c
@@ -3,5 +3,9 @@
 
 void main()
 {
-
+	puts("목원대 게임SW");
+	settextcol(DARK_SKYBLUE); // 글자색 변경; 색깔 상수는 LibConsole.h에 열거형 상수(enum)로 정의
+	puts("목원대 게임SW");
+	setbackcol(YELLOW); // 배경(background)색 변경; 색깔 상수는 LibConsole.h에 열거형 상수(enum)로 정의
+	puts("목원대 게임SW");
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "LibConsole.h 추가"
+git commit -m "글자색, 배경색 변경 방법"
 git push -u origin master
