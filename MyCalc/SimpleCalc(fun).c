7874b48984c0963fd05358a0257bee3b1f680bc8
mokwongame <ictplusud@gmail.com>
2023-05-08 12:16:15 +0900

    실습 #4(함수로 만든 단순 계산기) 예시

---
+++ MyCalc/SimpleCalc(fun).c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "LibConsole.h"
+
+void startCalc()
+{
+	settextcol(RED);
+	puts("=======================");
+	puts("| Simple Calculator   |");
+	puts("| 개발일: 2023-05-8   |");
+	puts("=======================");
+	settextcol(WHITE);
+}
+
+void stopCalc()
+{
+
+}
+
+void main()
+{
+	startCalc(); // 시작 화면
+	while (1) // 계속 반복
+	{
+		// 실수 입력: a, b
+		printf("실수 a: ");
+		double a = getdbl();
+		printf("실수 b: ");
+		double b = getdbl();
+		printf("입력한 a = %g, b = %g\n\n", a, b);
+
+		// 연산자 입력: op
+		printf("연산자(+-*/%%) 입력: ");
+		char op = _getche();
+		printf("\n입력한 연산자 = %c\n", op);
+
+		// 연산자에 따라 계산
+		if (op == '+')
+		{
+			double ans = a + b;
+			printf("\n%g + %g = %g\n", a, b, ans);
+		}
+		else if (op == '%') // 나머지 연산은 정수만 가능
+		{
+			int ans = (int)a % (int)b; // (int)는 int로 형 변환(type cast)
+			printf("\n%d %% %d = %d\n", (int)a, (int)b, ans);
+		}
+	}
+	stopCalc();
+}
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "ASCII 출력 함수 추가"
+git commit -m "실습 #4(함수로 만든 단순 계산기) 예시"
 git push -u origin master
