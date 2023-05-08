a5d1f893fcab6ad6f47d3048e0a927a85c5c2355
mokwongame <ictplusud@gmail.com>
2023-05-08 12:32:26 +0900

    실습 #4(함수로 만든 단순 계산기)에 함수 4개 추가

--- MyCalc/SimpleCalc(fun).c
+++ MyCalc/SimpleCalc(fun).c
@@ -16,6 +16,28 @@ void stopCalc()
 
 }
 
+char inputOp()
+{
+	printf("연산자(+-*/%%) 입력: ");
+	char op = _getche();
+	printf("\n입력한 연산자 = %c\n", op);
+	return op; // op을 반환(출력); op의 자료형이 char
+}
+
+void calc(double a, double b, char op)
+{
+	if (op == '+')
+	{
+		double ans = a + b;
+		printf("\n%g + %g = %g\n", a, b, ans);
+	}
+	else if (op == '%') // 나머지 연산은 정수만 가능
+	{
+		int ans = (int)a % (int)b; // (int)는 int로 형 변환(type cast)
+		printf("\n%d %% %d = %d\n", (int)a, (int)b, ans);
+	}
+}
+
 void main()
 {
 	startCalc(); // 시작 화면
@@ -29,21 +51,10 @@ void main()
 		printf("입력한 a = %g, b = %g\n\n", a, b);
 
 		// 연산자 입력: op
-		printf("연산자(+-*/%%) 입력: ");
-		char op = _getche();
-		printf("\n입력한 연산자 = %c\n", op);
+		char op = inputOp();
 
 		// 연산자에 따라 계산
-		if (op == '+')
-		{
-			double ans = a + b;
-			printf("\n%g + %g = %g\n", a, b, ans);
-		}
-		else if (op == '%') // 나머지 연산은 정수만 가능
-		{
-			int ans = (int)a % (int)b; // (int)는 int로 형 변환(type cast)
-			printf("\n%d %% %d = %d\n", (int)a, (int)b, ans);
-		}
+		calc(a, b, op);
 	}
 	stopCalc();
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "실습 #4(함수로 만든 단순 계산기) 예시"
+git commit -m "실습 #4(함수로 만든 단순 계산기)에 함수 4개 추가"
 git push -u origin master
