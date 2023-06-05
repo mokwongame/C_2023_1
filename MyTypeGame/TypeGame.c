32fd1ed83ce1660564d55cac52d01d42767f33b4
mokwongame <ictplusud@gmail.com>
2023-06-05 12:24:17 +0900

    타자 게임에서 문장 출력과 사용자 입력 추가

--- MyTypeGame/TypeGame.c
+++ MyTypeGame/TypeGame.c
@@ -13,5 +13,13 @@ void main()
 "Success is not final, failure is not fatal: It is the courage to continue that counts.",
 	"If you can dream it, you can do it."};
 	int nQuote = (int)(sizeof(quotes) / STR_MAX);
-	int nRand = randrange(0, nQuote); // 0, 1, 2, ..., nQuote-1 범위의 난수 발생
+	char strIn[STR_MAX]; // char의 배열 = 문자열; 문자열 입력을 받기 위해 메모리 공간을 STR_MAX만큼 확보
+
+	while (1)
+	{
+		int nRand = randrange(0, nQuote); // 0, 1, 2, ..., nQuote-1 범위의 난수 발생
+		puts(quotes[nRand]);
+		puts("\n\n문자열을 입력하세요:");
+		gets_s(strIn, STR_MAX - 1); // _s: safe(안전한); gets_s: safe gets; 문자열의 끝에는 0 문자가 들어가야 함; '0'가 아닌 숫자 0
+	}
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "실습 #6(타자 게임) 시작"
+git commit -m "타자 게임에서 문장 출력과 사용자 입력 추가"
 git push -u origin master
