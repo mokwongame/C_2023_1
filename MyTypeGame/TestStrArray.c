784c68ca027982ddfa7d44e2b149e07fc194050d
mokwongame <ictplusud@gmail.com>
2023-06-05 11:54:57 +0900

    문자열 입력 함수 추가

--- MyTypeGame/TestStrArray.c
+++ MyTypeGame/TestStrArray.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>	// 표준 입출력을 위한 헤더 파일
+#include <string.h> // 문자열 처리 위한 헤더 파일
 
-#define STR_MAX	(200)	// 문자열의 문자 개수의 최대값
+#define STR_MAX	(500)	// 문자열의 문자 개수의 최대값
 
 void main()
 {
@@ -18,4 +19,9 @@ void main()
 	int nQuote = (int)(sizeQuotes / STR_MAX);
 	printf("\n size = %lld\n", sizeQuotes);
 	printf("\n # of quotes = %d", nQuote);
+
+	char strIn[STR_MAX]; // char의 배열 = 문자열; 문자열 입력을 받기 위해 메모리 공간을 STR_MAX만큼 확보
+	puts("\n\n문자열을 입력하세요:");
+	gets_s(strIn, STR_MAX-1); // _s: safe(안전한); gets_s: safe gets; 문자열의 끝에는 0 문자가 들어가야 함; '0'가 아닌 숫자 0
+	puts(strIn);
 }
