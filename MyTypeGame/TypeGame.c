e61a3d5f392dcad3155437eee5412eb39321c678
mokwongame <ictplusud@gmail.com>
2023-06-12 10:55:03 +0900

    득점 계산 기능 추가

--- MyTypeGame/TypeGame.c
+++ MyTypeGame/TypeGame.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h> // abs() 사용
 #include "LibGameTool.h"
 
 #define STR_MAX	(500)	// 문자열의 문자 개수의 최대값
@@ -16,6 +18,7 @@ char strIn[STR_MAX]; // char의 배열 = 문자열; 문자열 입력을 받기 위해 메모리 공�
 void initGame();
 int printRandStr();
 void inputStr();
+int calcScore(int nRand);
 
 void main()
 {
@@ -24,6 +27,7 @@ void main()
 	{
 		int nRand = printRandStr();
 		inputStr();
+		int score = calcScore(nRand);
 	}
 }
 
@@ -46,4 +50,21 @@ void inputStr()
 {
 	puts("\n\n문자열을 입력하세요:");
 	gets_s(strIn, STR_MAX - 1); // _s: safe(안전한); gets_s: safe gets; 문자열의 끝에는 0 문자가 들어가야 함; '0'가 아닌 숫자 0
+}
+
+int calcScore(int nRand)
+{
+	int nRandLen = (int)strlen(quotes[nRand]); // 난수 문자열의 길이
+	int nInputLen = (int)strlen(strIn); // 입력 문자열의 길이
+	int nMinLen = (nRandLen < nInputLen) ? nRandLen : nInputLen; // 최소 문자열을 선택: ( ) ? :
+	//printf("\n문자열: %d, %d\n", nRandLen, nInputLen);
+	int score = 0;
+	for (int i = 0; i < nMinLen; i++)
+	{
+		if (quotes[nRand][i] == strIn[i]) score++;
+		else score--;
+	}
+	printf("득점: %d\n", score);
+	return score;
+	//return score - abs(nRandLen - nInputLen);
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "함수 선언으로 함수 정의를 분리"
+git commit -m "득점 계산 기능 추가"
 git push -u origin master
